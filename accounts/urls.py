f218df6d47719dc7e98bb2b46a21670039ce0dc6
Damian McNulty <damianmcnulty@me.com>
2022-09-26 06:38:22 +0100

    update

--- accounts/urls.py
+++ accounts/urls.py
@@ -3,9 +3,9 @@ from . import urls_reset
 from .views import index, register, profile, logout, login
 
 urlpatterns = [
-    url(r'^register/$', register, name='register'),
-    url(r'^profile/$', profile, name='profile'),
-    url(r'^logout/$', logout, name='logout'),
-    url(r'^login/$', login, name='login'),
-    url(r'^password-reset/', include(urls_reset)),
+    path(r'^register/$', register, name='register'),
+    path(r'^profile/$', profile, name='profile'),
+    path(r'^logout/$', logout, name='logout'),
+    path(r'^login/$', login, name='login'),
+    path(r'^password-reset/', include(urls_reset)),
 ]
