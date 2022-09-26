fe35f65a7f8a9b7d0a32a114e13946415a5a5ef4
Damian McNulty <damianmcnulty@me.com>
2022-09-26 05:58:14 +0100

    update

--- accounts/urls_reset.py
+++ accounts/urls_reset.py
@@ -1,4 +1,4 @@
-from django.conf.urls import url
+from django.urls import url
 from django.core.urlresolvers import reverse_lazy
 from django.contrib.auth.views import password_reset, password_reset_done, password_reset_confirm, password_reset_complete
 
