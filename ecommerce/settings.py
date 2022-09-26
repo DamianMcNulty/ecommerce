6f7cc8ae1dbcb73d0da02b992a17ecf1db4018bf
Damian McNulty <damianmcnulty@me.com>
2022-09-26 07:21:11 +0100

    debug

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 
-DEBUG = False
+DEBUG = True
 
 #ALLOWED_HOSTS = ['damianmcnulty-ecommerce.herokuapp.com', '127.0.0.1']
 ALLOWED_HOSTS = [os.environ.get('C9_HOSTNAME'), os.environ.get('HOSTNAME'), 'localhost']
