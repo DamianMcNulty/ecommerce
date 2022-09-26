b29ca774713e42c7352ece9a5e19be119c803dfb
Damian McNulty <damianmcnulty@me.com>
2022-09-26 06:26:51 +0100

    update

--- accounts/urls_reset.py
+++ accounts/urls_reset.py
@@ -1,12 +1,12 @@
 from django.urls import re_path as url
 from django.urls import reverse_lazy
-from django.contrib.auth.views import PasswordResetView, PasswordResetDoneView, password_reset_confirm, password_reset_complete
+from django.contrib.auth.views import PasswordResetView, PasswordResetDoneView, PasswordResetConfirmView, PasswordResetCompleteView
 
 urlpatterns = [
     url(r'^$', PasswordResetView.as_view(),
         {'post_reset_redirect': reverse_lazy('password_reset_done')}, name='password_reset'),
     url(r'^done/$', PasswordResetDoneView.as_view(), name='password_reset_done'),
-    url(r'^(?P<uidb64>[0-9A-Za-z]+)-(?P<token>.+)/$', password_reset_confirm,
+    url(r'^(?P<uidb64>[0-9A-Za-z]+)-(?P<token>.+)/$', PasswordResetConfirmView.as_view(),
         {'post_reset_redirect': reverse_lazy('password_reset_complete')}, name='password_reset_confirm'),
-    url(r'^complete/$', password_reset_complete, name='password_reset_complete'),
+    url(r'^complete/$', PasswordResetCompleteView.as_view(), name='password_reset_complete'),
 ]
