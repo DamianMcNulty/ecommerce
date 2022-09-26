2adb001294ce99892fb33674f554d2e8ec150501
Damian McNulty <damianmcnulty@me.com>
2022-09-26 06:58:07 +0100

    update

--- ecommerce/urls.py
+++ ecommerce/urls.py
@@ -13,7 +13,7 @@ Including another URLconf
     1. Import the include() function: from django.conf.urls import url, include
     2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
-from django.urls import include, re_path
+from django.urls import include, re_path as url
 from django.contrib import admin
 from accounts import urls as urls_accounts
 from products import urls as urls_products
